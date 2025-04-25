94eb4945015f1aa1ccbe04409b7e419dad49008c
TootEgozy <tootegozy@gmail.com>
2025-04-25 13:48:02 +0300

    wip responsive design using tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Tourney:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..900&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </head>
 
   <div v-if="serverReady">
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/assets/router";
+import "./styles.css";
+import "./styles/tailwind.css";
 
 const app = createApp(App);
 app.use(router);
